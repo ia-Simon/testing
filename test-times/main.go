979ab6ada2b49e1903a923d7cc3b9f6e2424835b
Igor Simon <iasimon.sp@gmail.com>
2024-06-26 10:20:49 -0300

    test-times add sub test

--- test-times/main.go
+++ test-times/main.go
@@ -9,5 +9,5 @@ func main() {
 	now := time.Now().UTC()
 	twoWeeksAfter := time.Date(now.Year(), now.Month(), now.Day()+14, 0, 0, 0, 0, now.Location())
 
-	fmt.Println(now, twoWeeksAfter)
+	fmt.Println(now, twoWeeksAfter, twoWeeksAfter.Sub(time.Now().UTC()))
 }
